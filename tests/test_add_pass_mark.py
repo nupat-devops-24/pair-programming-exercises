6f5d7bd6140fc62dd951a01044ab39886e57479b
tomisile <tomioluwasile@gmail.com>
2025-04-25 10:33:04 +0100

    ops(test): test the add pass mark module

---
+++ tests/test_add_pass_mark.py
@@ -0,0 +1,11 @@
+"""This file contains test cases for the score upgrade program/feature."""
+
+
+import unittest
+import add_pass_mark
+
+
+class TestIncreaseScores(unittest.TestCase):
+    """To-do: class description."""
+
+    
