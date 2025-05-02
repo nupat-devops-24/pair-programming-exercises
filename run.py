e5b83417111bbcb5e3b7df761b34969348608f3c
Teeemy <onibonojemariam@gmail.com>
2025-05-02 18:19:00 +0200

    chore: run the python_version.py script

--- run.py
+++ run.py
@@ -3,19 +3,23 @@
 
 import time
 import add_pass_mark as boost
+import python_version as pv
 
 
-# Function calls
-e1 = time.time()
-boost.increase_score('students_results.csv')
-e2 = time.time()
-# Time taken for first function
-function_1_time = e2 - e1
-print("Function 1 runtime:", function_1_time)
+# # Function calls
+# e1 = time.time()
+# boost.increase_score('students_results.csv')
+# e2 = time.time()
+# # Time taken for first function
+# function_1_time = e2 - e1
+# print("Function 1 runtime:", function_1_time)
 
-e3 = time.time()
-boost.increase_score_v2()
-e4 = time.time()
-# Time taken for first function
-function_2_time = e4 - e3
-print("Function 2 runtime:", function_2_time)
+# e3 = time.time()
+# boost.increase_score_v2()
+# e4 = time.time()
+# # Time taken for first function
+# function_2_time = e4 - e3
+# print("Function 2 runtime:", function_2_time)
+
+
+pv.greet_and_calculate_once_no_loop()
