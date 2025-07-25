69c437cb66c49d39a2e99f2e5037a91ef7f4ab1c
Teeemy <onibonojemariam@gmail.com>
2025-07-25 15:13:15 +0200

    chore: modify bucket configuration file

--- my_static_website_infra/bucket_configuration.sh
+++ my_static_website_infra/bucket_configuration.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
 BUCKET_NAME="my-static-website-04"
-ENDPOINT=
+ENDPOINT=https://localhost.localstack.cloud:4566
+
+echo "Starting S3 Bucket Configuration for Static Website Hosting"
+
+# Set AWS CLI to use LocalStack
+export AWS_PROFILE=localstack
+
+# Enable static website
+aws --endpoint-url=$ENDPOINT s3 website s3://my-website-04/
+    --index-document index.html
+    --error-document error.html
+
+# Function to configure bucket policy
+aws s3api put-bucket-policy \
+    policy file://bucket-policy.json \
+
+echo "Bucket policy is successfully configured"
+
+
 
